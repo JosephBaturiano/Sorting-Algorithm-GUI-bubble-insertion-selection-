b1aac7ec5534bf9a4dcb3958e768ed4b72f2e620
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2023-02-12 22:35:56 +0800

    Defining a function for insertion sort

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ def bubble_sort():
                 print(random_num)
                 result.insert(tkinter.END, random_num)
                 result.insert(tkinter.END, ('\n'))
+
+# INSERTION SORT
+
+
+def insertion_sort():
+    print("\n\t\tTHE NUMBERS ARE SORTED NOW USING INSERTION SORT\n")
+    for i in range(1, len(random_num)):
+        anchor = random_num[i]
+        j = i - 1
+        while j >= 0 and anchor < random_num[j]:
+            random_num[j+1] = random_num[j]
+            j = j - 1
+        random_num[j+1] = anchor
+        print(random_num)
+        result.insert(tkinter.END, random_num)
+        result.insert(tkinter.END, ('\n'))
