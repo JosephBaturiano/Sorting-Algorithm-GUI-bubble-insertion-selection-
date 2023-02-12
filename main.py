15bbb9c1dcc14bb7590fa9f3b2acb33a7e8fe547
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2023-02-12 22:35:05 +0800

    Defining a function for bubble sort

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ root.geometry("449x500")
 root.config(bg="gray58")
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
+
+# BUBBLE SORT
+
+
+def bubble_sort():
+    print("\n\t\tTHE NUMBERS ARE SORTED NOW USING BUBBLE SORT\n")
+    for i in range(len(random_num)-1, 0, -1):
+        for j in range(i):
+            if random_num[j] > random_num[j+1]:
+                swap = random_num[j]
+                random_num[j] = random_num[j+1]
+                random_num[j+1] = swap
+                print(random_num)
+                result.insert(tkinter.END, random_num)
+                result.insert(tkinter.END, ('\n'))
